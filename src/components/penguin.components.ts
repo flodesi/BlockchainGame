dd722cd45353fd3b960a871d49a1c7b36254647e
Yitian Zhao <ytz.zhao@mail.utoronto.ca>
2020-01-18 15:12:46 -0500

    penguins stuff

---
+++ src/components/penguin.components.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'penguin',
+  template: `
+  <img
+    width="40"
+    alt="Crypto Club Penguin"
+    src="assets/img/base_penguin.png"
+  />
+  `
+})
+
+export class Penguin {
+}

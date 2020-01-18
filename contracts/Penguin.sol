87497fd97133ac28dc0cd8cec33530283bbe4435
flodesi <jfoldesi57@gmail.com>
2020-01-18 15:16:13 -0500

    added babymaker function with rudamentary genetic algo

--- contracts/Penguin.sol
+++ contracts/Penguin.sol
@@ -10,28 +10,39 @@ contract Penguin is ERC721BasicToken {
         uint hat; // hats value range is 0-4
         uint birthtime; // number of seconds until birth of child, max value between parents is used, doubles after child born
     }
-    
+
     penguin[] public penguins;
     address public owner;
-    
+
     constructor() public {
         owner = msg.sender;
     }
-    
-    function createPenguin(string _name, address _to) public {
+
+    function createPenguin(string _name, uint color, uint size, uint hat, address _to) public {
         require(owner == msg.sender);
         uint id = penguins.length;
-        penguins.push(penguin(_name, 1, 1, 1, 1));
+        penguins.push(penguin(_name, color, size, hat, 1));
         _mint(_to, id);
     }
-    
-    // function babymaker(uint _penguinID, uint _targetID) onlyOwnerOf(_penguinID) public {
-    //     penguin storage penguin1 = penguins[_penguinID];
-    //     penguin storage penguin2 = penguins[_targetID];
-        
-        
-        
-    //     penguin1.birthtime = penguin1.birthtime * 2;
-    //     penguin2.birthtime = penguin2.birthtime * 2;
-    // }
+
+    function babymaker(uint _penguinID, uint _targetID) public {
+        penguin storage penguin1 = penguins[_penguinID];
+        penguin storage penguin2 = penguins[_targetID];
+
+        uint newColor;
+        uint newSize;
+        uint newHat;
+
+        newColor = (penguin1.color + penguin2.color)/2;
+        newSize = (penguin1.size + penguin2.size) / 2;
+        newHat = (penguin1.hat + penguin2.hat) / 2;
+
+        uint id = penguins.length;
+        penguins.push(penguin("yoloooo", newColor, newSize, newHat, 1));
+        _mint(msg.sender, id);
+
+
+        penguin1.birthtime = penguin1.birthtime * 2;
+        penguin2.birthtime = penguin2.birthtime * 2;
+    }
 }

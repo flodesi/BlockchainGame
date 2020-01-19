b8b7f323aef4ff7ba95e6d464b6e8ef0a89d5338
Cullen Pu <Cullenpu@gmail.com>
2020-01-19 02:52:51 -0500

    Changed generate button in enclosure.

--- src/components/canvas.component.ts
+++ src/components/canvas.component.ts
@@ -5,7 +5,7 @@ import { Penguin } from "./penguin";
   selector: "canvas-component",
   template: `
     <canvas #canvas width="750" height="400"></canvas>
-    <button (click)="play()">Generate</button>
+    <button (click)="play()" mat-raised-button color="primary">Generate</button>
   `,
   styles: [
     "canvas { border-width: 3px; border-style: double; border-color: #1C6EA4; }"
